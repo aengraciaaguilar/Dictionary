f18c3bb36f802ad0eecaf5d1d96ee525032796c1
aengraciaaguilar <aengraciaaguilar@gmail.com>
2022-11-12 20:56:06 +0800

    print the menu

--- Dictionary_Aguilar.py
+++ Dictionary_Aguilar.py
@@ -2,3 +2,10 @@
 # Name: Aengracia Aguilar
 # Course and Year: BSCOE 2-2
 
+# this is to print the MENU
+
+print("Menu:")
+print("1. -> Add an Item")
+print("2. -> Search")
+print("3. -> Exit (y/n?)")
+
